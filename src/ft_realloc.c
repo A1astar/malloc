8200c02c550167b191d2aab8b604dbc7d8317d9c
Alexandre Lacroix <alexandre.lacroix01@outlook.fr>
2026-01-27 12:23:39 +0100

    calloc done

--- src/ft_realloc.c
+++ src/ft_realloc.c
@@ -1,20 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   realloc.c                                          :+:      :+:    :+:   */
+/*   ft_realloc.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alacroix <alacroix@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 14:14:42 by alacroix          #+#    #+#             */
-/*   Updated: 2026/01/21 12:59:28 by alacroix         ###   ########.fr       */
+/*   Updated: 2026/01/27 12:20:10 by alacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/malloc.h"
 
+static inline size_t *get_memblock_metadatas(void *ptr)
+{
+	return (size_t *)((char *)ptr - align16(sizeof(size_t)));
+}
+
 void	*ft_realloc(void *ptr, size_t size)
 {
-	(void)ptr;
-	(void)size;
-	return NULL;
+	if(!ptr)
+		return ft_malloc(size);
+	if(size == 0)
+	{
+		ft_free(ptr);
+		return NULL;
+	}
+	size_t *memblock_metadata = get_memblock_metadatas(ptr);
+	size_t current_memblock_size = *memblock_metadata & ~1;
+	if(size <= current_memblock_size)
+		return ptr;
+	void *new_ptr = ft_malloc(size);
+	if(!new_ptr)
+		return NULL;
+	ft_memmove(new_ptr, ptr, current_memblock_size);
+	ft_free(ptr);
+	return new_ptr;
 }
