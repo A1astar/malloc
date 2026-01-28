29a6e9524d077310a632370c129a950f176030be
Alexandre Lacroix <alexandre.lacroix01@outlook.fr>
2026-01-28 12:35:13 +0100

    show_alloc_mem_ex: hexdump for arenas_lst done

--- src/ft_show_alloc_mem_ex.c
+++ src/ft_show_alloc_mem_ex.c
@@ -6,11 +6,59 @@
 /*   By: alacroix <alacroix@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 12:24:07 by alacroix          #+#    #+#             */
-/*   Updated: 2026/01/27 16:03:25 by alacroix         ###   ########.fr       */
+/*   Updated: 2026/01/28 12:33:43 by alacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/malloc.h"
 
-void	show_alloc_mem_ex(void)
-{}
+static void print_16_bytes(void *memory)
+{
+    ft_printf("[%p]: ", (char *)memory);
+    char *string_portion = (char *)memory;
+    for (size_t i = 0; i < 16; i++)
+    {
+        if(string_portion[i] == '\0')
+            ft_printf("00");
+        else
+            ft_printf("%x", string_portion[i]);
+        if(i % 1 == 0)
+            ft_printf(" ");
+    }
+    ft_printf("\n");
+}
+
+static void hexdump_mmap(void *memory, size_t size)
+{
+    ft_printf("Arena [%p] -> [%p]:\n", memory, (char *)memory + size);
+    for(size_t i = 0; i < size; i +=16)
+        print_16_bytes((char *)memory + i);
+    ft_printf("\n");
+}
+
+static void hexdump_arena_lst(t_arena_lst *first_node, char *type)
+{
+    ft_printf(BOLD "%s:\n"RESET, type);
+    t_arena_lst *current_node = first_node;
+    if (!current_node)
+    {
+        ft_printf(ITALIC "    Empty\n" RESET);
+        return;
+    }
+    while(true)
+    {
+        hexdump_mmap((void *)current_node, current_node->arena_size);
+        current_node = current_node->next_arena;
+        if(current_node == first_node)
+            break;
+    }
+}
+
+void show_alloc_mem_ex(void)
+{
+    pthread_mutex_lock(&malloc_mutex);
+    hexdump_arena_lst(g_alloc_arenas.arenas_lst[TINY_ARENA], "TINY");
+    hexdump_arena_lst(g_alloc_arenas.arenas_lst[TINY_ARENA], "SMALL");
+    //hexdump_mmap_lst();
+    pthread_mutex_unlock(&malloc_mutex);
+}
